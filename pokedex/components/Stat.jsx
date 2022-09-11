a5faff66c28264fdfdddc3b982ef1143bbbe2acc
Hamza Amar <rhamzax@gmail.com>
2022-09-11 03:31:01 -0600

    Line up titles with progress bar

--- pokedex/components/Stat.jsx
+++ pokedex/components/Stat.jsx
@@ -6,7 +6,7 @@ const Stat = ({stat, name, width} ) => {
   }
   return (
     <>
-      <span className='justify-self-end px-5 whitespace-nowrap'>{name}</span>
+      <span className='justify-self-end px-5 py-2 whitespace-nowrap'>{name}</span>
       <div className="w-full bg-gray-200 rounded-md dark:bg-gray-700 my-2 col-span-2 ">
           <div className={`bg-red-400 text-base font-medium text-white text-right py-1 leading-none rounded-md`} style={widthStyle}>
             <span className='px-1'>{stat.base_stat}</span>
