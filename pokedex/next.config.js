d5417a43f72e19db6d5e00d2f82db052bae8a942
Hamza Amar <rhamzax@gmail.com>
2022-09-11 03:26:30 -0600

    Remove scrollRestoration

--- pokedex/next.config.js
+++ pokedex/next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: {
-    scrollRestoration: true,
-  },
+  // experimental: {
+  //   scrollRestoration: true,
+  // },
 }
 
 module.exports = nextConfig
