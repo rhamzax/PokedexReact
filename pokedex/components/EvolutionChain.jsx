4007990bbfcd2a4e93755916c3bdd1ddf2f1e05d
Hamza Amar <rhamzax@gmail.com>
2022-09-18 13:56:12 -0600

    Add keys to props

--- pokedex/components/EvolutionChain.jsx
+++ pokedex/components/EvolutionChain.jsx
@@ -16,19 +16,23 @@ const EvolutionChain = ({ evolutionChainUrl }) => {
     let evolutionList = getEvolutionSpecies(evolution)
 
     let items = evolutionList.map(evolution => {
-      if (evolution.length) {
+      if (evolution.length > 0) {
         return (
-          <div className="grid grid-flow-col">
+          <div className="grid grid-flow-col" key={item.id}>
             {evolution.map(item => <Evolution key={item.id} name={item.species_name} min_level={item.min_level} trigger_name={item.trigger_name} item={item.item} id={item.id} />)}
           </div>
         )
       }
-      return <Evolution key={evolution.id} name={evolution.species_name} min_level={evolution.min_level} trigger_name={evolution.trigger_name} item={evolution.item} id={evolution.id} />
+      return (
+        <div key={item.id}>
+            <Evolution key={evolution.id} name={evolution.species_name} min_level={evolution.min_level} trigger_name={evolution.trigger_name} item={evolution.item} id={evolution.id} />
+        </div> 
+      )   
     })
 
 
     return (
-      <>{items}</>
+      <div>{items}</div>
     )
   }
   else {
