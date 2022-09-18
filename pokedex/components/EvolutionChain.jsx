ac284cb34931f4c70f3df1144e85eb335000fa28
Hamza Amar <rhamzax@gmail.com>
2022-09-18 13:19:26 -0600

    Add multiple path evolutions

--- pokedex/components/EvolutionChain.jsx
+++ pokedex/components/EvolutionChain.jsx
@@ -13,59 +13,6 @@ const EvolutionChain = ({ evolutionChainUrl }) => {
     fetchEvolution()
 
   }, [])
-<<<<<<< Updated upstream
-  const getIndexFromURL = (url) => {
-    url = url.slice(42)
-    url = url.substring(0, url.length - 1)
-    return url
-  }
-
-  const getEvolutionSpecies = () => {
-    if (evolution) {
-
-      let evoChain = [];
-      let evoData = evolution.chain;
-
-      do {
-        let numberOfEvolutions = evoData.evolves_to.length;
-        let evoDetails = evoData.evolution_details[0]
-        let id = getIndexFromURL(evoData.species.url)
-        
-        evoChain.push({
-          "species_name": evoData.species.name,
-          "min_level": !evoDetails ? 1 : evoDetails.min_level,
-          "trigger_name": !evoDetails ? null : evoDetails.trigger.name,
-          "item": !evoDetails ? null : evoDetails.item,
-          "id": !id ? null: id
-        });
-
-        if (numberOfEvolutions > 1) {
-          for (let i = 1; i < numberOfEvolutions; i++) {
-            evoDetails = evoData.evolves_to[i].evolution_details[0]
-            id = getIndexFromURL(evoData.evolves_to[i].species.url)
-            evoChain.push({
-              "species_name": evoData.evolves_to[i].species.name,
-              "min_level": !evoDetails ? 1 : evoDetails.min_level,
-              "trigger_name": !evoDetails ? null : evoDetails.trigger.name,
-              "item": !evoDetails ? null : evoDetails.item,
-              "id": !id ? null: id
-            });
-          }
-        }
-
-        evoData = evoData.evolves_to[0];
-
-      } while (evoData != undefined && evoData.hasOwnProperty('evolves_to'));
-      return evoChain;
-    }
-  }
-  
-  if (evolution) {
-    let evolutionList = getEvolutionSpecies()
-    console.log(evolutionList)
-    return (
-      evolutionList.map(evolution => <Evolution name={evolution.species_name} min_level = {evolution.min_level} trigger_name={evolution.trigger_name} item={evolution.item} id={evolution.id}/>)
-=======
   if (evolution) {
     let evolutionList = getEvolutionSpecies(evolution)
 
@@ -83,7 +30,6 @@ const EvolutionChain = ({ evolutionChainUrl }) => {
 
     return (
       <>{items}</>
->>>>>>> Stashed changes
     )
   }
   else {
