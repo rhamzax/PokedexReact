4dee350edb1db0c18c7009bbace5b574783fc815
Hamza Amar <rhamzax@gmail.com>
2022-09-18 13:36:29 -0600

    Delete past evolution logic

--- pokedex/util/getEvolution.js
+++ pokedex/util/getEvolution.js
@@ -8,38 +8,6 @@ export const getEvolutionSpecies = (evolution) => {
   let evoChain = [];
   let evoData = evolution.chain;
 
-  // do {
-  //   let numberOfEvolutions = evoData.evolves_to.length;
-  //   let evoDetails = evoData.evolution_details[0];
-  //   let id = getIndexFromURL(evoData.species.url);
-
-  //   evoChain.push({
-  //     species_name: evoData.species.name,
-  //     min_level: !evoDetails ? 1 : evoDetails.min_level,
-  //     trigger_name: !evoDetails ? null : evoDetails.trigger.name,
-  //     item: !evoDetails ? null : evoDetails.item,
-  //     id: !id ? null : id,
-  //   });
-
-  //   if (numberOfEvolutions > 1) {
-  //     let evoSet = []
-  //     for (let i = 0; i < numberOfEvolutions; i++) {
-  //       evoDetails = evoData.evolves_to[i].evolution_details[0];
-  //       id = getIndexFromURL(evoData.evolves_to[i].species.url);
-  //       evoSet.push({
-  //         species_name: evoData.evolves_to[i].species.name,
-  //         min_level: !evoDetails ? 1 : evoDetails.min_level,
-  //         trigger_name: !evoDetails ? null : evoDetails.trigger.name,
-  //         item: !evoDetails ? null : evoDetails.item,
-  //         id: !id ? null : id,
-  //       });
-  //     }
-  //     evoChain.push(evoSet)
-  //   }
-
-  //   evoData = evoData.evolves_to[0];
-  // } while (evoData != undefined && evoData.hasOwnProperty("evolves_to"));
-
   while (evoData != undefined && evoData.hasOwnProperty("evolves_to")) {
     let numberOfEvolutions = evoData.evolves_to.length;
     let evoDetails = evoData.evolution_details[0];
