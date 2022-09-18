c1a3c3e700a75f3ac75b84f7f0de28b89a5c540e
Hamza Amar <rhamzax@gmail.com>
2022-09-18 15:30:35 -0600

    Style each evolution

--- pokedex/components/Evolution.jsx
+++ pokedex/components/Evolution.jsx
@@ -1,18 +1,64 @@
 import React from 'react'
 import Image from 'next/image'
-const Evolution = ({name, min_level, trigger_name, item, id}) => {
+import Link from 'next/link';
+import { BsArrowDown } from 'react-icons/bs';
+
+const Evolution = ({name, min_level, trigger_name, item, id, index}) => {
   let image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
   return (
-    <div>{id}
-    <Image
-            className="object-contain"
-            width={100}
-            height={100}
-            src={image}
-            alt="evolutions"
-      />
-    </div>
+    <Link href={`/pokemon/${name}`}>
+      <div className='grid justify-items-center capitalize mx-5 my-3'>
+        <div className='flex flex-col justify-center items-center text-sm'>
+          {ArrowShow(index, trigger_name, min_level, item)} 
+        </div>
+
+        <Image
+                className="object-contain"
+                width={100}
+                height={100}
+                src={image}
+                alt="evolutions"
+          />
+        <div>
+          {`#${id} \n ${name}`}
+        </div>
+      </div>
+    </Link>
+    
   )
 }
+
+const ArrowShow = (key, trigger_name, min_level, item) => {
+  if(key > 0){
+    if(trigger_name == 'use-item'){
+      return (
+        <>
+        {`${trigger_name} ${item["name"]}`}
+        <BsArrowDown className='text-4xl'/>
+        </>
+        )
+    }
+    else if(trigger_name == 'trade'){
+      return (
+        <>
+        {`${trigger_name}`}
+        <BsArrowDown className='text-4xl'/>
+        </>
+      )
+    }
+    else if(trigger_name == "level-up"){
+      return (
+        <>
+        {`${trigger_name} ${min_level}+`}
+        <BsArrowDown className='text-4xl'/>
+        </>
+        )
+    }  
+  }
+  else{
+    <></>
+  }
+}
+
 
 export default Evolution
--- pokedex/components/EvolutionChain.jsx
+++ pokedex/components/EvolutionChain.jsx
@@ -15,24 +15,27 @@ const EvolutionChain = ({ evolutionChainUrl }) => {
   if (evolution) {
     let evolutionList = getEvolutionSpecies(evolution)
 
-    let items = evolutionList.map(evolution => {
-      if (evolution.length > 0) {
+    let items = evolutionList.map((evolution, index) => {
+      if (evolution.length) {
         return (
           <div className="grid grid-flow-col" key={evolution}>
-            {evolution.map(item => <Evolution key={item.id} name={item.species_name} min_level={item.min_level} trigger_name={item.trigger_name} item={item.item} id={item.id} />)}
+            {evolution.map(item => <Evolution key={item.id} name={item.species_name} min_level={item.min_level} trigger_name={item.trigger_name} item={item.item} id={item.id} index={index}/>)}
           </div>
         )
       }
       return (
         <div key={evolution.id}>
-            <Evolution key={evolution.id} name={evolution.species_name} min_level={evolution.min_level} trigger_name={evolution.trigger_name} item={evolution.item} id={evolution.id} />
+            <Evolution key={evolution.id} name={evolution.species_name} min_level={evolution.min_level} trigger_name={evolution.trigger_name} item={evolution.item} id={evolution.id} index={index}/>
         </div> 
       )   
     })
 
 
     return (
-      <div>{items}</div>
+      <div className="grid justify-items-center">
+        <span className="text-xl bg-red-400 text-white px-4">Evolutions</span>
+        {items}
+      </div>
     )
   }
   else {
